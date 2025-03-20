4d782b299057c9a6f42b881c7a453de97452da8f
Vembe Sajila Rajil <svembe@gmail.com>
2025-03-20 06:11:47 -0400

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./assets/styles/App.css";
 import PeriodicTable from "./components/PeriodicTable";
 import Legend from "./components/Legend";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <PeriodicTable />
       <Legend />
     </div>
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title: React.FC = () => {
+  return (
+    <h1 style={{ fontSize: "48px", textAlign: "center" }}>Periodic Table</h1>
+  );
+};
+
+export default Title;
