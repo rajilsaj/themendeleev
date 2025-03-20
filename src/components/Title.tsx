31ad77ba2892530a4cc1848b43d8e3e40f4bd20c
Vembe Sajila Rajil <svembe@gmail.com>
2025-03-20 06:47:23 -0400

    Switching this project to Mando

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,25 @@ import React from "react";
 
 const Title: React.FC = () => {
   return (
-    <h1 style={{ fontSize: "48px", textAlign: "center" }}>Periodic Table</h1>
+    <>
+      <h1 style={{ fontSize: "48px", textAlign: "center" }}>Periodic Table</h1>
+      <section className="quote-section">
+        <blockquote>
+          "With more than 10³² possible designs, there are more data structure
+          designs than stars in the sky."
+        </blockquote>
+        <cite>
+          —{" "}
+          <a
+            href="https://www.cidrdb.org/cidr2021/papers/cidr2021_paper17.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            *The Periodic Table of Data Structures*, Harvard CIDR 2021
+          </a>
+        </cite>
+      </section>
+    </>
   );
 };
 
